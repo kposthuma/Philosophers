81aa4915a74b3d806294f0836e36e1f486390991
Koen Posthuma <kposthum@f0r4s16.codam.nl>
2023-05-18 14:56:16 +0200

    norme

--- philo/src/philo_eat_sleep.c
+++ philo/src/philo_eat_sleep.c
@@ -6,7 +6,7 @@
 /*   By: kposthum <kposthum@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/02 16:15:56 by kposthum      #+#    #+#                 */
-/*   Updated: 2023/05/18 14:45:04 by kposthum      ########   odam.nl         */
+/*   Updated: 2023/05/18 14:56:05 by kposthum      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,12 +53,8 @@ void	*philo_eat(t_philos *strc, size_t id, size_t id2)
 	pthread_mutex_unlock(&strc->lock);
 	if (action_loop(strc, id, strc->time_to_eat) == false)
 		return (NULL);
-	// pthread_mutex_lock(&strc->phils[id]->fork_lock);
 	strc->phils[id]->fork = 0;
-	// pthread_mutex_unlock(&strc->phils[id]->fork_lock);
-	// pthread_mutex_lock(&strc->phils[id2]->fork_lock);
 	strc->phils[id2]->fork = 0;
-	// pthread_mutex_unlock(&strc->phils[id2]->fork_lock);
 	pthread_mutex_lock(&strc->lock);
 	strc->phils[id]->meals_eaten++;
 	pthread_mutex_unlock(&strc->lock);
@@ -80,6 +76,5 @@ void	*philo_sleep(t_philos *strc, size_t id)
 		printf("%llu\t%lu\tis thinking\n", get_time() - strc->start_time,
 			strc->phils[id]->philo_id);
 	pthread_mutex_unlock(&strc->lock);
-	usleep(50);
 	return (NULL);
 }
